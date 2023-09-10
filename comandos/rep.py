77c7a6d1737d16d0adda85c484e9381856a262b8
aldov <aldovasquez2014@gmail.com>
2023-09-10 12:20:53 -0600

    rep mbr

--- comandos/rep.py
+++ comandos/rep.py
@@ -2,6 +2,7 @@ import os
 from elementos.disco import *
 from elementos.mbr import Mbr
 from comandos.mount import particiones_montadas
+from elementos.ebr import Ebr
 
 def execute_rep(args):
     if args.name == "mbr":
@@ -27,11 +28,11 @@ def reporte_mbr(ruta, id):
         if elemento_encontrado == None:
             print("No se encontro la particion")
         else:
+            inicioEBR = -1
             mbrDisco = Mbr()
             obtenerDatosDisco(elemento_encontrado[2], 0, mbrDisco)
             mbrDisco.display_info()
 
-            
             #generando codigo .dot del mbr
             dot = "digraph mbr{\n"
             dot += "a0 [shape=none label=<\n"
@@ -40,6 +41,24 @@ def reporte_mbr(ruta, id):
             dot += f"<TR><TD bgcolor=\"yellow\">mbr_tamano</TD><TD bgcolor=\"yellow\">{mbrDisco.get_tamano()}</TD></TR>\n"
             dot += f" <TR><TD bgcolor=\"yellow\">mbr_fecha_creacion</TD><TD bgcolor=\"yellow\">{mbrDisco.get_time()}</TD></TR>\n"
             dot += f"<TR><TD bgcolor=\"yellow\">mbr_disk_signature</TD><TD bgcolor=\"yellow\">{mbrDisco.get_dsk_signature()}</TD></TR>\n"
+            
+            #generando codigo .dot de los ebr en caso existan
+            if mbrDisco.particion1.type == b'e':
+                dot += dotExtendida(mbrDisco.particion1)
+                inicioEBR = mbrDisco.particion1.start
+            elif mbrDisco.particion2.type == b'e':
+                dot += dotExtendida(mbrDisco.particion2)
+                inicioEBR = mbrDisco.particion2.start
+            elif mbrDisco.particion3.type == b'e':
+                dot += dotExtendida(mbrDisco.particion3)
+                inicioEBR = mbrDisco.particion3.start
+            elif mbrDisco.particion4.type == b'e':
+                dot += dotExtendida(mbrDisco.particion4)
+                inicioEBR = mbrDisco.particion4.start
+
+            if inicioEBR != -1:
+                dot += dotLogica(elemento_encontrado[2], inicioEBR)
+            
             dot += "</TABLE>>];\n"
             dot += "}"
 
@@ -53,3 +72,31 @@ def reporte_mbr(ruta, id):
             
     except Exception as e:
         print(f"Error: {e}")
+
+def dotExtendida(particion):
+    dot = ""
+    dot += " <TR><TD bgcolor=\"yellow\">PARTICION EXTENDIDA</TD></TR>\n"
+    dot += f"<TR><TD bgcolor=\"yellow\">part_status</TD><TD bgcolor=\"yellow\">{particion.status}</TD></TR>\n"
+    dot += f"<TR><TD bgcolor=\"yellow\">part_type</TD><TD bgcolor=\"yellow\">{particion.type}</TD></TR>\n"
+    dot += f"<TR><TD bgcolor=\"yellow\">part_fit</TD><TD bgcolor=\"yellow\">{particion.fit}</TD></TR>\n"
+    dot += f"<TR><TD bgcolor=\"yellow\">part_start</TD><TD bgcolor=\"yellow\">{particion.start}</TD></TR>\n"
+    dot += f"<TR><TD bgcolor=\"yellow\">part_size</TD><TD bgcolor=\"yellow\">{particion.s}</TD></TR>\n"
+    dot += f"<TR><TD bgcolor=\"yellow\">part_name</TD><TD bgcolor=\"yellow\">{particion.name}</TD></TR>\n"
+    return dot
+
+def dotLogica(rutaDisco, puntero):
+    with open(rutaDisco, "rb") as discoAbierto:
+        discoAbierto.seek(puntero)
+        ebr = Ebr()
+        obtenerDatosDisco(rutaDisco, puntero, ebr)
+        dot = ""
+        dot += " <TR><TD bgcolor=\"yellow\">PARTICION LOGICA</TD></TR>\n"
+        dot += f"<TR><TD bgcolor=\"yellow\">part_status</TD><TD bgcolor=\"yellow\">{ebr.status}</TD></TR>\n"
+        dot += f"<TR><TD bgcolor=\"yellow\">part_fit</TD><TD bgcolor=\"yellow\">{ebr.fit}</TD></TR>\n"
+        dot += f"<TR><TD bgcolor=\"yellow\">part_start</TD><TD bgcolor=\"yellow\">{ebr.start}</TD></TR>\n"
+        dot += f"<TR><TD bgcolor=\"yellow\">part_size</TD><TD bgcolor=\"yellow\">{ebr.s}</TD></TR>\n"
+        dot += f"<TR><TD bgcolor=\"yellow\">part_next</TD><TD bgcolor=\"yellow\">{ebr.next}</TD></TR>\n"
+        dot += f"<TR><TD bgcolor=\"yellow\">part_name</TD><TD bgcolor=\"yellow\">{ebr.name}</TD></TR>\n"
+        if ebr.next != -1:
+            dot += dotLogica(rutaDisco, ebr.next)
+        return dot
